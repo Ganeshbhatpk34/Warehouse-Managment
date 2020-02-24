c18b0cf82cc07a8399ef8f6214817d2fd39102f4
Ganesh <ganesh@rhbussolutions.com>
2020-02-24 15:17:10 +0530

    deleted Compo

--- app/src/main/java/com/probatus/rhbus/warehouse/Interface/INavigationFragment.java
+++
@@ -1,8 +0,0 @@
-package com.probatus.rhbus.warehouse.Interface;
-
-/**
- * Created by ganapathi on 23/1/20.
- */
-
-public interface INavigationFragment {
-}
--- app/src/main/java/com/probatus/rhbus/warehouse/JsonParse/HttpResponse.java
+++
@@ -1,9 +0,0 @@
-package com.probatus.rhbus.warehouse.JsonParse;
-
-/**
- * Created by ganapathi on 12/4/19.
- */
-
-public interface HttpResponse<JSONObject>  {
-    void onResponse(org.json.JSONObject response);
-}
